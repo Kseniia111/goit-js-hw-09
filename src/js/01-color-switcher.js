7785adc27c5b6a9d49d44b7796905ea3dd4d70a4
Kseniia111 <kseniya.markovych@gmail.com>
2023-08-04 16:58:31 +0300

    Task 1 start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,7 @@
+const bodyChangeColor = document.querySelector('body');
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
 
