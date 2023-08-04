6e6ff581721799c01ebb85d7bd4b7202c36ff955
Kseniia111 <kseniya.markovych@gmail.com>
2023-08-04 18:56:00 +0300

    Task one Working Code

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,4 +4,24 @@ const buttonStop = document.querySelector('[data-stop]');
 
 let intervalId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+buttonStop.setAttribute('disabled', '');
+
+buttonStart.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  buttonStop.removeAttribute('disabled');
+
+  intervalId = setInterval(() => {
+    bodyChangeColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+buttonStop.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  buttonStart.removeAttribute('disabled');
+
+  clearInterval(intervalId);
+});
